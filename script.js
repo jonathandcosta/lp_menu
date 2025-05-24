6e3e207b88339d8039c52c4226c72149c7064735
Jonathan Costa <jonathanduartecosta@gmail.com>
2025-05-24 17:02:33 -0300

    feat: insert slow scroll

--- script.js
+++ script.js
@@ -13,3 +13,21 @@ faqItems.forEach((item) => {
     });
   });
 });
+
+// Smooth Scrolling for Anchor Links
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const targetId = this.getAttribute('href');
+    if (targetId === '#') return;
+
+    const targetElement = document.querySelector(targetId);
+    if (targetElement) {
+      window.scrollTo({
+        top: targetElement.offsetTop - 80,
+        behavior: 'smooth',
+      });
+    }
+  });
+});
